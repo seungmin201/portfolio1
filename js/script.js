ba43b87816128f4db74a72fb64ecf9dcdd4519a8
seungmin201 <167734597+seungmin201@users.noreply.github.com>
2024-05-31 14:44:42 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ var swiper = new Swiper(".contents_slide", {
       pagination: {
         clickable: true,
       },
-    FreeMode:{enabled:true},
+      freeMode:{enabled:true},
       breakpoints: {
         450: { slidesPerView: 1.4,},
         500: { slidesPerView: 1.5,},
